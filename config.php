9d3ad1e4ccba2d566e26cf00f44df7ffe7bb42d3
Sławek Skiba <slawek.skiba@gmail.com>
2023-06-02 00:32:41 +0200

    Restore config

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+//WSL
+define("db_servername","127.0.0.1");
+define("db_username", "test-api");
+define("db_password", "password");
+define("db_name","api_test");
+
+
+// JWT configuration
+$jwtSecretKey = 'jasdkh-345sdf';
+$jwtIssuer = 'sskiba-software';
+
