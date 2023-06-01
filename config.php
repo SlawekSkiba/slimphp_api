5cbf49a938f1a66e7bd3fc51f64d68cd62873cbf
Sławek Skiba <slawek.skiba@gmail.com>
2023-06-02 00:29:31 +0200

    remove pass

--- config.php
+++ config.php
@@ -1,11 +1,5 @@
 <?php
 
-// awardspace
-// define("db_servername","fdb1029.awardspace.net");
-// define("db_username", "4316374_sskiba");
-// define("db_password", "ba@32JHP5]PacFe%");
-// define("db_name","4316374_sskiba");
-
 //WSL
 define("db_servername","127.0.0.1");
 define("db_username", "test-api");
